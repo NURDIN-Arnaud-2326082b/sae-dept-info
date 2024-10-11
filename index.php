aa3bcb4f2971aac213478a8ef9bc3de35e41aae0
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2024-10-11 11:05:04 +0200

    avancée dans le déboggage de factorisation du routeur

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 require_once './src/Autoloader.php';
 App\src\Autoloader::register();
 
-use App\src\controllers\pages\HomepageController;
+
 use App\src\database\DatabaseConnection;
 use App\src\controllers\pages\error404Controller;
 
@@ -14,9 +14,8 @@ $routePath = implode('/', $segments);
 if ($routePath === '') {
 $routePath = '/';
 }
-
 $controllerSegment = $segments[0] ?? 'authentication';
-$actionSegment = $segments[1] ?? 'ConnectUser';
+$actionSegment = $segments[1] ?? '';
 $methodSegment = $segments[2] ?? 'defaultMethod';
 
 $database = DatabaseConnection::getInstance();
@@ -34,29 +33,25 @@ exit();
 if ($controllerSegment === '') {
     $controllerSegment = 'Homepage';
 }
+
 $controllerName = ucfirst($controllerSegment) . 'Controller';
-// Convertir le nom de l'action (par exemple, "index" devient "indexAction")
 $actionName = $actionSegment . 'Action';
-// Générer le namespace complet du contrôleur
 
 $controllerClass = "App\\src\\controllers\\pages\\{$controllerName}";
-// Vérifier si la classe du contrôleur existe
 if (class_exists($controllerClass)) {
-    // Instancier le contrôleur
     $controller = new $controllerClass();
-    var_dump($controller);
-    var_dump($actionName);
-    // Vérifier si la méthode demandée existe dans ce contrôleur
-    if (method_exists($controller, $actionName)) {
-        // Appeler la méthode correspondante
+    if ($actionName != 'Action'  && method_exists($controller, $actionName)) {
         $controller->$actionName();
-    } else {
-        // Si l'action n'existe pas, appeler la méthode par défaut (404)
+        $controller->$methodSegment();
+    }
+    elseif ($actionName === 'Action') {
+        $controller->$methodSegment();
+    }
+    else {
         $errorController = new Error404Controller();
         $errorController->defaultMethod();
     }
 } else {
-    // Si le contrôleur n'existe pas, rediriger vers la page 404
     $errorController = new Error404Controller();
     $errorController->defaultMethod();
 }
