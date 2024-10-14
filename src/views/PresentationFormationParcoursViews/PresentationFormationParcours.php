af8a0a8338663382431371292d53612ce21f8325
FABRE-Alexandre-2326043 <alexandre.fabre.2@etu.univ-amu.fr>
2024-10-14 15:10:52 +0200

    Organisation des études

--- src/views/PresentationFormationParcoursViews/PresentationFormationParcours.php
+++ src/views/PresentationFormationParcoursViews/PresentationFormationParcours.php
@@ -30,10 +30,28 @@ class PresentationFormationParcours
                         <h3> Parcours B : Dépoiement d'appliations communicantes et sécurisées </h3>
                             <p> Ce parcours prépare les étudiants à la mise en place et à la sécurité des systèmes d'information <br>
                                 et des applications. Il forme aux métiers d'administrateur système/réseaux ou applicatifs, cybersécurité, <br>
-                                DevOps, intégrateur d'application, ... </p>
+                                DevOps, intégrateur d'application, ...  <br> <br> </p>
+
                 <h2> Organisation des études</h2>
                     <h3> Structure et organisation </h3>
-                        <p> Répartie sur 3 années, la formation représente 1800 heures encadrées, auxquelles s'ajoutent 600 heures de projet encadré </p>
+                        <p> Répartie sur 3 années, la formation représente 1800 heures encadrées, auxquelles s'ajoutent 600 heures <br>
+                            de projet encadré et à minima 22 à 26 semaines de stage et/ou d'alternance.
+                            Cette formation requiert des savoir-être et des savoir-faire, tant académiques que professionnels, <br>
+                            organisés autour du développement des six grandes compétences qui caractérisent le B.U.T. Informatique : <br> <br> </p>
+                            <ul> <li> Réaliser un développement d’application </li>
+                                 <li> Optimiser des solutions informatiques </li>
+                                 <li> Administrer des systèmes informatiques communicants complexes </li>
+                                 <li> Gérer des données de l’information </li>
+                                 <li> Conduire un projet </li>
+                                 <li> Travailler dans une équipe informatique </li> </ul> <br> <br>
+
+                            <p> En particulier, le premier parcours repose sur une consolidation de la compétence "Optimiser des <br>
+                                solutions informatiques", alors que le second parcours s’appuie sur un renforcement de la compétence <br>
+                                "Administrer des systèmes informatiques communicants complexes". Sur le site d’Aix-en-Provence, les <br>
+                                deux parcours aborderont par ailleurs l’utilisation d’outils d’IA liés respectivement aux questions <br>
+                                d’optimisation et de sécurisation. D’un point de vue pratique, une partie du cursus pourra être réalisé <br>
+                                en alternance ; une sortie diplômante à bac + 2 restera possible via l’obtention du D.U.T Informatique. <br>
+                                Enfin, il sera en principe envisageable de suivre 1 ou 2 semestres à l’étranger pendant le cursus.</p>
 
 
             <p> <a href="https://iut.univ-amu.fr/fr/formations/bachelor-universitaire-de-technologie/but-informatique/but-info-aix"> En savoir plus </a> </p>
