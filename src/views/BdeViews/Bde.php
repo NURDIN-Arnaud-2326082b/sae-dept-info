ec99251265be3c0ebacbd5b0169cbf327cb494ed
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2024-10-15 12:05:33 +0200

    suite implémentation de la page sur le bde

--- src/views/BdeViews/Bde.php
+++ src/views/BdeViews/Bde.php
@@ -64,7 +64,27 @@ class Bde
                     </div>
                 </div>
             </section>
+            <section class="department-content">
+                <h2>Les évènements phares du BDE</h2>
+                <div class="events">
+                    <ul>
+                        <li><img src="/assets/images/img.png" alt="Soirée">
+                            <h3>Soirée de rentrée</h3>
+                            <p>La soirée de rentrée du BDE est un évènement incontournable pour tous les étudiants du département. C'est l'occasion de se retrouver et de faire connaissance avec les nouveaux étudiants.</p></li>
+                        <li> <img src="/assets/images/img.png" alt="Soirée">
+                            <h3>Soirée Halloween</h3>
+                            <p>La soirée Halloween est un évènement organisé par le BDE pour fêter Halloween. C'est l'occasion de se déguiser et de passer un moment agréable entre étudiants.</p>
+                        </li>
+                        <li>                    <img src="/assets/images/img.png" alt="Soirée">
+                            <h3>Soirée de fin d'année</h3>
+                            <p>La soirée de fin d'année est un évènement organisé par le BDE pour fêter la fin de l'année universitaire. C'est l'occasion de</p></li>
+                    </ul>
+                </div>
 
+
+
+
+            </section>
         </main>
         <?php
         (new Layout('Présentation du département', ob_get_clean()))->show();
