b3912b96c0bd65e280aaf4b4f936798af3eb5bb0
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2024-12-17 15:23:36 +0100

    ajout template pour liens

--- src/controllers/PageControlleur.php
+++ src/controllers/PageControlleur.php
@@ -88,42 +88,43 @@ class PageControlleur
             }
             echo '</div>';
         }
-        else {
-            echo '<section id="content" class="department-content">';
-            $cpt = 1;
-            foreach ($content as $ct) {
-                switch ($ct['type']) {
-                    case 'texte':
-                        echo '<div class="intro">';
-                        echo '<h2>' . $ct['title'] . '</h2>';
-                        echo '<p>' . $ct['content'] . '</p>';
-                        echo '</div>';
-                        break;
-                    case 'list' . $cpt:
-                        $cpt2 = 0;
-                        echo '<div class="features-grid">';
-                        foreach ($content as $ct2) {
-                            if ($ct2['type'] == 'list' . $cpt) {
-                                echo '<div class="feature">';
-                                echo '<img src="/assets/images/img.png" alt="Innovation">';
-                                echo '<h3>' . $ct2['title'] . '</h3>';
-                                echo '<p>' . $ct2['content'] . '</p>';
-                                echo '</div>';
-                                $cpt2++;
-                            }
+        echo '<section id="content" class="department-content">';
+        $cpt = 1;
+        foreach ($content as $ct) {
+            switch ($ct['type']) {
+                case 'texte':
+                    echo '<div class="intro">';
+                    echo '<h2>' . $ct['title'] . '</h2>';
+                    echo '<p>' . $ct['content'] . '</p>';
+                    echo '</div>';
+                    break;
+                case 'list' . $cpt:
+                    $cpt2 = 0;
+                    echo '<div class="features-grid">';
+                    foreach ($content as $ct2) {
+                        if ($ct2['type'] == 'list' . $cpt) {
+                            echo '<div class="feature">';
+                            echo '<img src="/assets/images/img.png" alt="Innovation">';
+                            echo '<h3>' . $ct2['title'] . '</h3>';
+                            echo '<p>' . $ct2['content'] . '</p>';
+                            echo '</div>';
+                            $cpt2++;
                         }
-                        for ($i = 0; $i < $cpt2; $i++) {
-                            array_shift($content);
-                        }
-                        echo '</div>';
-                        $cpt++;
-                        break;
-                    case 'titre':
-                        echo '<h2>' . $ct['title'] . '</h2>';
-                        break;
-                    default:
-                        break;
-                }
+                    }
+                    for ($i = 0; $i < $cpt2; $i++) {
+                        array_shift($content);
+                    }
+                    echo '</div>';
+                    $cpt++;
+                    break;
+                case 'titre':
+                    echo '<h2>' . $ct['title'] . '</h2>';
+                    break;
+                case 'lien':
+                    echo '<div><a href="' . $ct['link'] . '" >'.$ct['content'].'</a></div>';
+                    break;
+                default:
+                    break;
             }
         }
     }
