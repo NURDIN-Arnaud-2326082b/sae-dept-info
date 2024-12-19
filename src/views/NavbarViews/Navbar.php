39dcf98d66fd9d33f233e27c5a74054a781a5cc2
FABRE-Alexandre-2326043 <alexandre.fabre.2@etu.univ-amu.fr>
2024-12-19 15:35:15 +0100

    replacement des éléments de la navbar

--- src/views/NavbarViews/Navbar.php
+++ src/views/NavbarViews/Navbar.php
@@ -1,57 +1,50 @@
 <?php
-    namespace App\src\views\NavbarViews;
+namespace App\src\views\NavbarViews;
+
+/**
+ * Navbar Class
+ *
+ * Provides methods to render the navbar of the application.
+ */
+class Navbar
+{
     /**
-     * Navbar Class
-     *
-     * Provides methods to render the navbar of the application.
+     * Affiche la navbar.
      */
-    class Navbar
-    {
+    public function show($user): void {
+        ?>
+        <link rel="stylesheet" href="/assets/styles/navbar.css">
 
-        /**
-         * Affiche la navbar.
-         */
-        public function show($user): void {
+        <nav class="navbar">
+            <a href="http://localhost:8080">
+                <img src="/assets/images/logo_amu.png" alt="Logo amu noir" class="logo">
+            </a>
 
+            <!-- Nom de l'utilisateur ou "Invité" -->
+            <h1><?= isset($user['name']) ? htmlspecialchars($user['name']) : 'Invité' ?></h1>
 
-            ?>
-            <link rel="stylesheet" href="/assets/styles/navbar.css">
+            <!-- Barre de recherche -->
+            <div class="search-bar">
+                <?php
+                $currentFile = basename($_SERVER['PHP_SELF']);
+                $isHomepage = ($currentFile === 'index.php');
 
+                if (!$isHomepage): ?>
+                    <label for="search"></label>
+                    <input type="text" id="search" placeholder="Recherche... (Ctrl + K)">
+                <?php endif; ?>
+            </div>
 
-            <nav class="navbar">
-                <a href="http://localhost:8080">
-                    <img src="/assets/images/logo_amu.png" alt="Logo amu noir" class="logo">
-                </a>
-                <h1><?= isset($user['name']) ? htmlspecialchars($user['name']) : 'Invité' ?></h1>
-                <div class="search-bar">
-                    <?php
-
-                    $currentFile = basename($_SERVER['PHP_SELF']);
-                    $isHomepage = ($currentFile === 'index.php');
-
-                    if (isset($user['name'])): ?>
-                        <a href="/logout">Se déconnecter</a>
-                  <?php  elseif (!$isHomepage): ?>
-                        <label for="search"></label>
-                        <input type="text" id="search" placeholder="Recherche... (Ctrl + K)">
-                    <?php else: ?>
-                        <a href="/login">Se connecter</a>
-                    <?php endif; ?>
-                </div>
-
-                <!-- Ajout du bouton "Menu" -->
-                <div class="menu-button">
-                    <?php if (isset($user['name'])): ?>
-                        <a href="/menu" class="btn btn-menu">Menu</a>
-                    <?php endif; ?>
-                </div>
-
-
-
-
-
-            </nav>
-
-            <?php
-        }
+            <!-- Actions -->
+            <div class="actions">
+                <?php if (isset($user['name'])): ?>
+                    <a href="/logout" class="btn btn-logout">Se déconnecter</a>
+                    <a href="/menu" class="btn btn-menu">Menu</a>
+                <?php else: ?>
+                    <a href="/login" class="btn btn-login">Se connecter</a>
+                <?php endif; ?>
+            </div>
+        </nav>
+        <?php
     }
+}
