f7b0f8c1a7eec1041b80d72b57e845ff41f3b5a5
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2025-01-12 22:05:18 +0100

    ajout video youtube

--- src/views/pages/Show.php
+++ src/views/pages/Show.php
@@ -340,6 +340,9 @@ class Show
                         echo '<a href="/PageControlleur/getPdf?id='.$ct['id_article'].'" download="fichier.pdf">Télécharger le PDF</a>';
                         echo '</div>';
                         break;
+                    case 'youtube':
+                        echo '<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/sVoBk3g-ZmA?si=_GRzxx9eprOXzGQ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>';
+                            break;
                     default:
                         break;
                 }
