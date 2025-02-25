8e98237638e4ca09661a74c1d4aca9477d50961b
FABRE-Alexandre-2326043 <alexandre.fabre.2@etu.univ-amu.fr>
2025-02-25 11:42:45 +0100

    amélioration du CSS

--- src/views/pages/Layout.php
+++ src/views/pages/Layout.php
@@ -45,6 +45,7 @@ class Layout
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="icon" type="image/png" href="/assets/images/favicon/favicon_amu.png" sizes="32x32" />
             <link rel="icon" type="image/png" href="/assets/images/favicon/favicon_amu.png" sizes="192x192" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         </head>
         <body>
         <header>
