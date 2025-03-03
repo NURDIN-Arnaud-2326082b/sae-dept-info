a668859c6779f2da5de3d7344e8a3a858c38b6cd
loebd <dorian.loeb@etu.univ-amu.fr>
2025-03-03 10:59:57 +0100

    rectification

--- src/views/pages/Footer.php
+++ src/views/pages/Footer.php
@@ -21,7 +21,7 @@ class Footer {
             <a href="/">
             <img src="/assets/images/logo_amu.png" alt="logo iut">
             </a>
-            <p>© 2024 Département Informatique - IUT d'Aix-Marseille</p>
+            <p>© 2025 Département Informatique - IUT d'Aix-Marseille</p>
         </footer>
 
         <?php
